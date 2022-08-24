668e349515aaf208b08e34d24e0dcb1d04235124
munag <munag@LAPTOP-CNRMAV0A>
2022-08-24 15:53:20 +0530

    Second update done

---
+++ src/test/java/SampleForGithub/SecondUpdate.java
@@ -0,0 +1,11 @@
+package SampleForGithub;
+
+public class SecondUpdate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second update is successfully done");
+
+	}
+
+}
